d958d6a7e125f8e0882a25ca12948ecb592a3153
durvesh8 <durveshmalpurecoc@gmail.com>
2023-07-11 14:54:42 -0400

    changed hyperparameters

--- ColossalAI/examples/language/gpt/titans/configs/gpt3_zero3_pp1d.py
+++ ColossalAI/examples/language/gpt/titans/configs/gpt3_zero3_pp1d.py
@@ -3,11 +3,11 @@ from model import GPT3_pipeline_hybrid, GPT3_pipeline_hybridgpt2small
 from colossalai.nn.optimizer import HybridAdam
 from colossalai.zero.shard_utils import TensorShardStrategy
 
-BATCH_SIZE = 192
+BATCH_SIZE = 10
 NUM_EPOCHS = 60
 SEQ_LEN = 2048
-NUM_MICRO_BATCHES = 192
-HIDDEN_SIZE = 12288
+NUM_MICRO_BATCHES = 2
+HIDDEN_SIZE = 768
 TENSOR_SHAPE = (BATCH_SIZE // NUM_MICRO_BATCHES, SEQ_LEN, HIDDEN_SIZE)
 
 # if you do no want zero, just comment out this dictionary
@@ -27,6 +27,6 @@ model = dict(type=GPT3_pipeline_hybridgpt2small, checkpoint=True, num_chunks=1,n
 # tensor parallel: modify size to set the tensor parallel size, usually the number of GPUs per node
 # for the current model implementation, mode can only be 1D or None
 parallel = dict(
-    pipeline=1,
+    pipeline=2,
     tensor=dict(size=1, mode='1d'),    # for the current model implementation, mode can only be 1D or None
 )
